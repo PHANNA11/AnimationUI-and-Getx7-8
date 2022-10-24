66e5ea89c43b5eef6ccf0a5ed2879c96618033ae
PHANNA11 <phaphanna2018@gmail.com>
2022-10-24 14:15:13 +0700

    public branch

---
+++ lib/screen/finger_prin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FingerPrin extends StatelessWidget {
+  const FingerPrin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
